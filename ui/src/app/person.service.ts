97af815ee50c74e4eecfe57abe365082ba193124
Matt Payne <Payne@MattPayne.org>
2023-01-05 20:05:48 -0600

    HTTP GET to persons api fetching json - TODO: handle the json

--- ui/src/app/person.service.ts
+++ ui/src/app/person.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import {from, Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonService {
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) { }
 
   getPersons(): Observable<string> {
+    return from(this.http.get<string>('/api/persons'));
+    /*
     const people = ['John', 'Jane', 'Joe'];
     return from(people);
+     */
   }
 }
